023b5d06e5cca669d9a449e93fae154bf702517e
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2022-12-06 22:15:59 +0100

    ajout du footer

---
+++ Projet/navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg" style="background-color: rgb(17, 31, 77)">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-between w-100 flex-wrap">
+                <div class="d-flex justify-content-center">
+                    <a class="navbar-brand text-white" href="#" style="margin-left: 30px; margin-right: 90px"><span class="fw-bold fs-4">Your</span> <span class="fs-5">Train</span> <span class="fs-4">™</span></a>
+                </div>
+
+                <div class="">
+                    <div class="" id="">
+                        <div class="" id="navbarSupportedContent">
+
+                            <ul class="navbar-nav d-flex flex-row justify-content-evenly ">
+                                <li class=" nav-item dropdown me-4 pe-4">
+                                    <a class="nav-link dropdown-toggle text-white" href="# " role="button " data-bs-toggle="dropdown" aria-expanded="false ">
+                                        <span class="material-symbols-outlined text-white"> account_circle </span>
+                                    </a>
+                                    <ul class="dropdown-menu ">
+                                        <li><a class="dropdown-item mt-2 " href="# ">Mon Profil</a></li>
+                                        <li><a class="dropdown-item mt-2 " href="# ">Mes réservations</a></li>
+                                        <li><a class="dropdown-item my-2 " href="# ">Se déconnecter</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item me-5">
+                                    <a class="nav-link " href="# "><span class="material-symbols-outlined text-white ">shopping_cart</span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </nav>
--- Projet/result.php
+++ Projet/result.php
@@ -106,6 +106,25 @@
             </div>
         </div>
     </section>
+    <footer style="background-color: rgb(188, 206, 248);">
+        <div class="container mt-5 pt-5">
+            <div class="d-flex flex-nowrap justify-content-center">
+                <a href="#" style="color: black;"><i class='bx bxl-facebook fs-1 mx-2'></i></a>
+                <a href="#" style="color: black;"><i class='bx bxl-twitter fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i class='bx bxl-instagram fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i class='bx bxl-youtube fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i class='bx bxl-whatsapp fs-1 mx-2' ></i></a>
+            </div>
+            <div>
+                <div>
+                    <p>YourTrain™ Voyages</p>
+                   <a href=""> <p> Réserver et planifier </p></a>
+                   <a href=""> <p> Gérer la réservation </p></a>
+                </div>
+            </div>
+
+        </div>
+    </footer>
 </body>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js " integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4 " crossorigin="anonymous "></script>
