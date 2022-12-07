a7d11a23ed718e161418a185bf6f4794b3a448ad
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2022-12-07 12:01:50 +0100

    Finalisation du Footer et ajout de navbar et section

--- Projet/navbar.php
+++ Projet/navbar.php
@@ -1,8 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Résultats de votre recherche disponibiltés des Trains</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="./result.css" />
+</head>
+
+<body style="background-color: rgb(242, 244, 247)">
 <nav class="navbar navbar-expand-lg" style="background-color: rgb(17, 31, 77)">
         <div class="container-fluid">
             <div class="d-flex justify-content-between w-100 flex-wrap">
                 <div class="d-flex justify-content-center">
-                    <a class="navbar-brand text-white" href="#" style="margin-left: 30px; margin-right: 90px"><span class="fw-bold fs-4">Your</span> <span class="fs-5">Train</span> <span class="fs-4">™</span></a>
+                    <a class="navbar-brand text-white" href="#" style="margin-left: 30px;"><span class="fw-bold fs-4">Your</span> <span class="fs-5">Train</span> <span class="fs-4">™</span></a>
                 </div>
 
                 <div class="">
@@ -10,7 +28,7 @@
                         <div class="" id="navbarSupportedContent">
 
                             <ul class="navbar-nav d-flex flex-row justify-content-evenly ">
-                                <li class=" nav-item dropdown me-4 pe-4">
+                                <li class=" nav-item dropdown me-3 pe-2">
                                     <a class="nav-link dropdown-toggle text-white" href="# " role="button " data-bs-toggle="dropdown" aria-expanded="false ">
                                         <span class="material-symbols-outlined text-white"> account_circle </span>
                                     </a>
@@ -29,3 +47,8 @@
                 </div>
             </div>
     </nav>
+    </body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js " integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4 " crossorigin="anonymous "></script>
+
+</html>
--- Projet/result.php
+++ Projet/result.php
@@ -99,7 +99,7 @@
                             <p class="px-2 fw-semibold ">1 passager</p>
                             <p class="px-2 fw-semibold ">A partir de</p>
                             <p class="px-2 fw-semibold ">115 MAD</p>
-                            <button id="btnReserv " type="submit " class="btn btn-dark px-3 ">Réserver</button>
+                            <button id="btnReserv" type="submit" class="btn btn-dark px-3">Réserver</button>
                         </div>
                     </div>
                 </div>
@@ -109,18 +109,33 @@
     <footer style="background-color: rgb(188, 206, 248);">
         <div class="container mt-5 pt-5">
             <div class="d-flex flex-nowrap justify-content-center">
-                <a href="#" style="color: black;"><i class='bx bxl-facebook fs-1 mx-2'></i></a>
-                <a href="#" style="color: black;"><i class='bx bxl-twitter fs-1 mx-2' ></i></a>
-                <a href="#" style="color: black;"><i class='bx bxl-instagram fs-1 mx-2' ></i></a>
-                <a href="#" style="color: black;"><i class='bx bxl-youtube fs-1 mx-2' ></i></a>
-                <a href="#" style="color: black;"><i class='bx bxl-whatsapp fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i id="bx1" class='bx bxl-facebook fs-1 mx-2'></i></a>
+                <a href="#" style="color: black;"><i id="bx2" class='bx bxl-twitter fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i id="bx3" class='bx bxl-instagram fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i id="bx4" class='bx bxl-youtube fs-1 mx-2' ></i></a>
+                <a href="#" style="color: black;"><i id="bx5" class='bx bxl-whatsapp fs-1 mx-2' ></i></a>
             </div>
-            <div>
-                <div>
-                    <p>YourTrain™ Voyages</p>
-                   <a href=""> <p> Réserver et planifier </p></a>
-                   <a href=""> <p> Gérer la réservation </p></a>
+            <div class="d-flex justify-content-center my-5 pb-4">
+                <div class="mx-4 px-2 text-center">
+                    <p class="fw-semibold fs-5">YourTrain™ Voyages</p>
+                   <a href="#" class="text-decoration-none text-dark"> <p class="lien"> Réserver et planifier </p></a>
+                   <a href="#" class="text-decoration-none text-dark"> <p class="lien"> Gérer la réservation </p></a>
                 </div>
+                <div class="mx-4 px-2 text-center">
+                    <p class="fw-semibold fs-5">Sites du Groupe</p>
+                    <a href="#" class="text-decoration-none text-dark"> <p class="lien"> YourTrain.ma </p></a>
+                   <a href="#" class="text-decoration-none text-dark"> <p class="lien">MyBus.ma </p></a>
+                </div>
+            </div>
+            <hr class="pb-5 text-center" id="line">
+            <div class="text-center pb-2">
+                <a href="#" class="text-decoration-none text-dark"><p class="lien" style="font-size: 13px;">Conditions générales de vente et d’utilisation du site</p></a>
+            </div>
+            <div class="pb-4 text-center" >
+                <img src="../images/cartes.png" alt="">
+            </div>
+            <div class="text-center py-2">
+                <p style="font-size: 10px;">© 2022 YourTrain™-Tous droits réservés</p>
             </div>
 
         </div>
